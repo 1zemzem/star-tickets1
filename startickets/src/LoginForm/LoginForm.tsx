852bd0cbf640c7fb198e9cf41d112323e6711776
1zemzem <1zemliakova@gmail.com>
2022-03-23 13:12:20 +0300

    loginFormStatic

--- startickets/src/LoginForm/LoginForm.tsx
+++ startickets/src/LoginForm/LoginForm.tsx
@@ -1,40 +1,78 @@
 import React from "react";
-import { useForm, Controller } from "react-hook-form";
-import { Input } from "@mui/material"
+import { useForm } from "react-hook-form";
 
-import "./LoginForm.css"
+import "./LoginForm.css";
 
 interface IFormInput {
   firstName: string;
   lastName: string;
-  iceCreamType: { label: string; value: string };
+  email: string;
+  password: string;
 }
 
 export const LoginForm = () => {
-  const { control, handleSubmit } = useForm<IFormInput>();
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors, isValid },
+  } = useForm<IFormInput>({ mode: "onBlur" });
 
   const onSubmit = (data: IFormInput) => {
     alert(JSON.stringify(data));
+    reset();
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <label>First Name</label>
-      <Controller
-        render={({ field }) => <Input {...field} className="materialUIInput" />}
-        name="firstName"
-        control={control}
-        defaultValue=""
-      />
-      <label>Last Name</label>
-      <Controller
-        render={({ field }) => <Input {...field} className="materialUIInput" />}
-        name="firstName"
-        control={control}
-        defaultValue=""
-      />
+    <div className="form">
+      <h1>Регистрация</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <label>First Name</label>
+          <input
+            {...register("firstName", { required: "! this line is required" })}
+            placeholder="Ivan"
+          />
+          {errors?.firstName && <p>{errors.firstName.message}</p>}
+        </div>
 
-      <input type="submit" />
-    </form>
+        <div>
+          <label>Last Name</label>
+          <input
+            {...register("lastName", { required: "! this line is required" })}
+            placeholder="Ivanov"
+          />
+          {errors?.lastName && <p>{errors.lastName.message}</p>}
+        </div>
+
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            {...register("email", { required: "! this line is required" })}
+            placeholder="koko1049@gmail.com"
+            type="email"
+          />
+          {errors?.email && <p>{errors.email.message}</p>}
+        </div>
+
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            {...register("password", {
+              required: "! this line is required",
+              minLength: {
+                value: 6,
+                message: "6 simbols minimum required",
+              },
+            })}
+            placeholder="6 simbols minimum"
+            type="password"
+          />
+          {errors?.password && <p>{errors.password.message}</p>}
+        </div>
+
+        <input type="submit" disabled={!isValid} />
+      </form>
+    </div>
   );
 };
